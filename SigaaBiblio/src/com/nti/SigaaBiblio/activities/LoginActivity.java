8a7f58f94a05302f54592d7d13d2971bd236defe
IronAraujo <ironaraujo@gmail.com>
2013-06-30 11:54:15 -0300

    Activity Menu criada, Portrait e Landscape

--- SigaaBiblio/src/com/nti/SigaaBiblio/activities/LoginActivity.java
+++ SigaaBiblio/src/com/nti/SigaaBiblio/activities/LoginActivity.java
@@ -4,14 +4,24 @@ import com.nti.SigaaBiblio.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements OnClickListener {
+	
+	Button login;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		login = (Button)findViewById(R.id.button1);
+		
+		login.setOnClickListener(this);
 	}
 
 	@Override
@@ -20,6 +30,13 @@ public class LoginActivity extends Activity {
 		
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
+	}
+	
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent(this, MenuActivity.class);
+		startActivity(intent);
+		
 	}
 	
 
