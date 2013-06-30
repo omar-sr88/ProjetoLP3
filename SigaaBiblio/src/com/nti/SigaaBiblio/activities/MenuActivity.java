7c6739613af85077e3821a09e0ae145fa3dee9ae
IronAraujo <ironaraujo@gmail.com>
2013-06-30 18:20:10 -0300

    Activity Situacao Usuario

--- SigaaBiblio/src/com/nti/SigaaBiblio/activities/MenuActivity.java
+++ SigaaBiblio/src/com/nti/SigaaBiblio/activities/MenuActivity.java
@@ -3,14 +3,31 @@ package com.nti.SigaaBiblio.activities;
 import android.os.Bundle;
 import com.nti.SigaaBiblio.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MenuActivity extends Activity {
-
+	Button situacao;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
+		
+		situacao = (Button)findViewById(R.id.button4);
+		
+		situacao.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MenuActivity.this, SituacaoUsuarioActivity.class );
+				startActivity(intent);
+				
+			}
+		});
+		
 	}
 
 	@Override
