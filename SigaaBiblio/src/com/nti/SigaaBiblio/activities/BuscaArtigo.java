0e23913a7f09486c5c19f089d33f14dc25256a18
Omar SR <omar.sr88@gmail.com>
2013-07-01 20:25:58 -0300

    alguns updates BuscaArtigo

---
+++ SigaaBiblio/src/com/nti/SigaaBiblio/activities/BuscaArtigo.java
@@ -0,0 +1,26 @@
+package com.nti.SigaaBiblio.activities;
+
+import com.nti.SigaaBiblio.R;
+import com.nti.SigaaBiblio.R.layout;
+import com.nti.SigaaBiblio.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BuscaArtigo extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_busca_artigo);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.busca_artigo, menu);
+		return true;
+	}
+
+}
