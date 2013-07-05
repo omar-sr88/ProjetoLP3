01039ab7399c62196661d07f9c9bfc7d6562b8ea
marcilioLemos <marcilio.cc.lemos@gmail.com>
2013-07-05 00:17:55 -0300

    resultado da busca, selecionar histórico (layout normal e land)

--- SigaaBiblio/src/com/nti/SigaaBiblio/activities/BuscaArtigoActivity.java
+++ SigaaBiblio/src/com/nti/SigaaBiblio/activities/BuscaArtigoActivity.java
@@ -4,14 +4,32 @@ import com.nti.SigaaBiblio.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class BuscaArtigoActivity extends Activity {
 
+	Button goResultados;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_busca_artigo);
+		
+		goResultados = (Button)findViewById(R.id.buttonBuscarArtigo);
+		
+		goResultados.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(BuscaArtigoActivity.this, ResultadoBuscaActivity.class );
+				startActivity(intent);
+				
+			}
+		});
 	}
 
 	@Override
