789c7667f71df08e7dd3decd87423ca4fe2c7c9a
Omar SR <omar.sr88@gmail.com>
2013-07-05 23:09:54 -0300

    navegacao do resultado do historico

--- SigaaBiblio/src/com/nti/SigaaBiblio/activities/HistoricoEmprestimosActivity.java
+++ SigaaBiblio/src/com/nti/SigaaBiblio/activities/HistoricoEmprestimosActivity.java
@@ -6,14 +6,44 @@ import com.nti.SigaaBiblio.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HistoricoEmprestimosActivity extends Activity {
 
+	Button retornaPesquisaButton;
+	Button retornaMenuButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_historico_emprestimos);
+		
+		retornaMenuButton = (Button) findViewById(R.id.retornarMenu);
+		retornaPesquisaButton = (Button) (Button) findViewById(R.id.retornarPesquisa);
+		
+		retornaMenuButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(HistoricoEmprestimosActivity.this, MenuActivity.class );
+				startActivity(intent);
+
+			}
+		});
+		retornaPesquisaButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(HistoricoEmprestimosActivity.this, SelecionarHistoricoActivity.class );
+				startActivity(intent);
+
+			}
+		});
+		
 	}
 
 	@Override
