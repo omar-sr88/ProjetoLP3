90b4ba73006aa98142ad0e22c462a2055883b040
IronAraujo <ironaraujo@gmail.com>
2013-07-06 12:29:15 -0300

    Adicao de pasta de animacao; Alteracao na activity: Historico..., Adicionado o exemplo no layout dessa activity de como se aplica.

--- SigaaBiblio/src/com/nti/SigaaBiblio/activities/SituacaoUsuarioActivity.java
+++ SigaaBiblio/src/com/nti/SigaaBiblio/activities/SituacaoUsuarioActivity.java
@@ -6,21 +6,83 @@ import com.nti.SigaaBiblio.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
+import android.widget.ViewSwitcher;
 
 public class SituacaoUsuarioActivity extends Activity {
-
+    ViewFlipper page;
+    Animation animFlipInForeward;
+    Animation animFlipOutForeward;
+    Animation animFlipInBackward;
+    Animation animFlipOutBackward;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_situacao_usuario);
+		
+		page = (ViewFlipper)findViewById(R.id.viewFlipper1);	      
+		animFlipInForeward = AnimationUtils.loadAnimation(this, R.anim.flipin);
+        animFlipOutForeward = AnimationUtils.loadAnimation(this, R.anim.flipout);
+        animFlipInBackward = AnimationUtils.loadAnimation(this, R.anim.flipin_reverse);
+        animFlipOutBackward = AnimationUtils.loadAnimation(this, R.anim.flipout_reverse);	
+        page.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				return gestureDetector.onTouchEvent(event);
+			}
+		});
+	}
+	
+	private void SwipeRight(){
+    	page.setInAnimation(animFlipInBackward);
+		page.setOutAnimation(animFlipOutBackward);
+		page.showPrevious();
+    }
+    
+    private void SwipeLeft(){
+    	page.setInAnimation(animFlipInForeward);
+		page.setOutAnimation(animFlipOutForeward);
+		page.showNext();
+    }
+    
+    @Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+    	return gestureDetector.onTouchEvent(event);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.situacao_usuario, menu);
-		return true;
-	}
+	SimpleOnGestureListener simpleOnGestureListener 
+    = new SimpleOnGestureListener(){
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+
+			float sensitvity = 20;
+			if((e1.getX() - e2.getX()) > sensitvity){
+				SwipeLeft();
+			}else if((e2.getX() - e1.getX()) > sensitvity){
+				SwipeRight();
+			}
+			
+			return true;
+		}
+    	
+    };
+    
+    GestureDetector gestureDetector
+	= new GestureDetector(simpleOnGestureListener);
 
 }
